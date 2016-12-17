2571dd51a4571ecd46ec627596c0aaee0b3427c5
kwiscion <kwiscion@gmail.com>
2016-12-17 17:59:49 +0100

    votes correlation

--- src/R/R.R
+++ src/R/R.R
@@ -1,12 +1,23 @@
 library(tidyverse)
 library(plotly)
 
-path <- '../../data/2016-12-03-WBO_lista_glosow_2016.csv'
-data <- read.csv(path, header = T, sep = ';')
+data_path <- '../../data/'
+votes_2016 <- paste0(data_path, '2016-12-03-WBO_lista_glosow_2016.csv')
+proj_2016 <- paste0(data_path, 'projects_2016.csv')
+data <- read.csv(votes_2016, header = T, sep = ';')
+proj_data <- read.csv(proj_2016, header = F, sep = ',', stringsAsFactors = F) %>%
+  select(V1,V2,V3,V5,V6,V7) %>%
+  rename(id = V1,
+         status = V2,
+         glosy = V3,
+         budzet = V5,
+         obszar = V6,
+         kategoria = V7) %>%
+  mutate(kategoria = ifelse(kategoria == 'do 750 000 zł', 
+                            '750k',
+                            ifelse(kategoria == 'do 250 000 zł', '250k', '1kk')),
+         obszar = ifelse(obszar == 'zieleń/rekreacja', 'ziele�/rekreacja', obszar))
 
-data %>%
-  group_by(Zrodlo, Plec) %>%
-  mutate(Wiek_m = Wiek/mean(Wiek))
 
 data_o <- data %>%
   select(c(Ogolno_1, Ogolno_2)) %>%
@@ -35,6 +46,42 @@ results_second <- data_o %>%
   inner_join(results %>%
                rename(cnt_pierwszy = total,
                       pct_pierwszy = pct))
+
+results_projects <- results_second %>%
+  left_join(proj_data %>% select(id, status, obszar), by=c('pierwszy'='id')) %>%
+  rename(status_1 = status,
+         obszar_1 = obszar) %>%
+  left_join(proj_data %>% select(id, status, obszar), by=c('drugi'='id')) %>%
+  rename(status_2 = status,
+         obszar_2 = obszar) %>%
+  mutate(rel_bin = cut(rel/median(rel), breaks = c(0, 0.7, 1.3, Inf), 
+                       labels = c('Negative', 'Neutral', 'Positive')))
+results_projects[is.na(results_projects)] <- 0
+
+results_projects %>%
+  group_by(status_1, status_2) %>%
+  summarise(negative = mean(rel_bin == 'Negative'),
+            neutral = mean(rel_bin == 'Neutral'),
+            positive = mean(rel_bin == 'Positive')) %>%
+  filter(status_1 != 0)
+
+results_projects %>%
+  group_by(status_1, status_2) %>%
+  summarise(mean(rel),
+            median(rel)) %>%
+  filter(status_1 != 0)
+
+ggplot(results_projects %>%
+         filter(obszar_1=='drogowe'), 
+       aes(rel, color=paste(obszar_1, obszar_2))) + 
+  geom_density() + 
+  scale_x_log10() + 
+  theme_bw()
+ggplotly()
+
+
+
+
 
 View(results_second %>% 
        filter(cnt_pierwszy >= 100 & cnt_drugi >= 100) %>% 
